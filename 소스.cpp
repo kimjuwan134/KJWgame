24dd8b11eaa39cb93375b02d0cd4e1430e286d5c
kimjuwan134 <139342292+kimjuwan134@users.noreply.github.com>
2023-09-07 18:01:38 +0900

    1

--- 소스.cpp
+++ 소스.cpp
@@ -1,11 +1,26 @@
-#include <stdio.h>
+#include<cstdio>
+
+int arr[100];
 
 int main()
 {
-	int A, B;
+	int n;
+	int target;
+	int input;
+	int count = 0;
+	scanf("%d", &n);
 
-	while (scanf_s("%d %d", &A, &B) != -1)
-	printf("%d\n", A + B);
+	for (int i = 0; i < n; i++) {
+		scanf("%d", arr + i);
+	}
+
+	scanf("%d", &target);
+
+	for (int i = 0; i < n; i++) {
+		if (arr[i] == target) count += 1;
+	}
+
+	printf("%d\n", count);
 
 	return 0;
 }
