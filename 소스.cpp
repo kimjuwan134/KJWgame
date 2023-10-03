c31ea25c8add9d28a0ac1e7650feb46f6361f150
kimjuwan134 <139342292+kimjuwan134@users.noreply.github.com>
2023-10-04 02:07:11 +0900

    1

--- 소스.cpp
+++ 소스.cpp
@@ -1,17 +1,43 @@
 #include <iostream>
-#include <string>
 
 using namespace std;
 
 
 int main()
 {
-	cout << "         ,r'\"7" << endl;
-	cout << "r`-_   ,'  ,/" << endl;
-	cout << " \\. \". L_r'" << endl;
-	cout << "   `~\\/" << endl;
-	cout << "      |" << endl;
-	cout << "      |";
+	int N;
+
+	cin >> N;
+
+	for (int i = 0; i < 2 * N - 1; i++)
+	{
+		for (int j = i; j < N; j++)
+		{
+			cout << " ";
+		}
+		for (int k = N - i; k <= N + i; k++)
+		{
+			cout << "*";
+		}
+		if (i >= N)
+		{
+			for (int j = i; j >= N; j--)
+			{
+				cout << " ";
+			}
+			for (int k = i - N; k <= N; k++)
+			{
+				cout << "*";
+			}
+		}
+		else if (i == N)
+		{
+			continue;
+		}
+		cout << endl;
+	}
+
+
 
 	return 0;
 }
