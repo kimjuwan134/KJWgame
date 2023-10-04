1a705cf03c1cab5db8b1279213ead4d78e3a058d
kimjuwan134 <139342292+kimjuwan134@users.noreply.github.com>
2023-10-04 23:30:43 +0900

    1

--- 소스.cpp
+++ 소스.cpp
@@ -5,35 +5,40 @@ using namespace std;
 
 int main()
 {
-	int N;
+	string str;
+	int abc[26] = { 0, };
+	int count = 0;
 
-	cin >> N;
+	cin >> str;
 
-	for (int i = 0; i < N; i++)
+	for (int i = 0; i < str.length(); i++)
 	{
-		for (int j = i; j < N - 1; j++)
-		{
-			cout << " ";
-		}
-		for (int k = 1; k < 2 * (i+1); k++)
-		{
-			cout << "*";
-		}
-		cout << endl;
+		if (str[i] < 'a') abc[str[i] - 'A']++;
+		else abc[str[i] - 'a']++;
 	}
 
-	for (int i = N; i < 2*N-1; i++)
+	int max = 0, max_index = 0;
+
+	for (int i = 0; i < 26; i++)
 	{
-		for (int k = N; k <= i; k++)
+		if (max < abc[i])
 		{
-			cout << " ";
+			max = abc[i];
+			max_index = i;
 		}
-		for (int j = 4; j <= 2*(2*N-i); j++)
-		{
-			cout << "*";
-		}
-		cout << endl;
 	}
+
+	for (int i = 0; i < 26; i++)
+	{
+		if (max == abc[i]) count++;
+	}
+
+	if (count > 1) cout << "?";
+	else cout << char(max_index + 65);
+
+
+
+
 
 
 	return 0;
