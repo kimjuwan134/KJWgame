9cecfd854c955058c896b330e428946888a4b69a
kimjuwan134 <139342292+kimjuwan134@users.noreply.github.com>
2023-10-12 17:09:53 +0900

    1

--- 소스.cpp
+++ 소스.cpp
@@ -1,33 +1,17 @@
 #include <iostream>
-#include <vector>
 
 using namespace std;
 
 
 int main()
 {
-	int max = 0;
-	int a, b;
-	int arr[9][9];
+	int N, B;
+	cin >> N >> B;
 
-	for (int i = 0; i < 9; i++)
+	for (int i = 0; i < B; i++)
 	{
-		for (int j = 0; j < 9; j++)
-		{
-			cin >> arr[i][j];
 
-			if (max <= arr[i][j])
-			{
-				max = arr[i][j];
-				a = i + 1;
-				b = j + 1;
-			}
-		}
 	}
-
-	cout << max << endl << a << " " << b;
-
-
 
 
 	return 0;
